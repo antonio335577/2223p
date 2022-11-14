5a7632d9be622fe13ecb4c4ceb2be02f93715505
ama <amorenoa28@educarex.es>
2022-11-14 20:32:14 +0100

    Primer commint

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author MiPc
+ *
+ */
+module ProyectoGit2 {
+}
---
+++ src/paq/Prueba.java
@@ -0,0 +1,10 @@
+package paq;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
