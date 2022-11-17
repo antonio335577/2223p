50e1650ffede68c8485a8899bdada1317cd8bd43
ama <amorenoa28@educarex.es>
2022-11-17 18:17:30 +0100

    actu

--- src/paq/Prueba.java
+++ src/paq/Prueba.java
@@ -3,7 +3,8 @@ package paq;
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		System.out.println("Hola mundo");
+		
 		
 
 	}
